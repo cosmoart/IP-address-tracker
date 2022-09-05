1115e2955d0fc08bb2f9602f92f167143b2a7796
CosmoArt <cosmohydra17@gmail.com>
2022-09-04 20:30:18 -0500

    table info & styles

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,28 @@ const Title = styled.h1`
 	color: white;
 `
 
-const TableInfo = styled.table`
+const TableInfo = styled.ul`
 	margin: auto;
 	background: white;
 	border-radius: 10px;
 	padding: 1rem;
 	text-align: left;
-
-	thead th{
-		padding-right: 15px;
-		opacity: .8;
+	display: flex;
+	gap: 2rem;
+	justify-content: space-around;
+	list-style: none;
+	text-align: left;
+	z-index: 10;
+	span{
+		display: block;
 	}
-	tbody th{
-		font-size: 22px;
+	span:last-of-type{
+		font-size: 20px;
+		font-weight: bold;
+	}
+	@media screen and (max-width: 770px) {
+		flex-direction: column;
+		text-align: center;
 	}
 	`
 
@@ -39,6 +48,7 @@ const FormInfo = styled.form`
 		background: var(--VeryDarkGray);
 		border-radius: 0 10px 10px 0;
 		padding: 10px 20px;
+		cursor: pointer;
 	}
 	button:hover{
 		opacity: .8;
@@ -48,48 +58,41 @@ const FormInfo = styled.form`
 function App() {
 	const [query, setQuery] = useState("192.212.174.101");
 	const [info, setInfo] = useState({ "ip": "", "location": "", "timezone": "", "isp": "" });
-	const [loading, setLoading] = useState();
+	const [loading, setLoading] = useState(true);
 
 
 	function getInfo(q = "8.8.8.8") {
-		axios.get(`https://geo.ipify.org/api/v2/country?apiKey=at_KWi4iJ3mHvSf60uE9YdyDDLUo2owD&ipAddress=${q}`)
+		setLoading(true)
+		axios.get(`https://geo.ipify.org/api/v2/country,city?apiKey=at_KWi4iJ3mHvSf60uE9YdyDDLUo2owD&ipAddress=${q}`)
 			.then(function (response) {
-				// handle success
+				setInfo(response.data);
+				Map.invalidateSize(true)
 				console.log(response.data)
 			})
 			.catch(function (error) {
-				// handle error
+				setInfo({ "error": error.message })
 				console.log(error);
 			})
-			.then(function () {
-				// always executed
-			});
+			.then(() => setLoading(false));
 	}
 
 	useEffect(() => {
-		var container = L.DomUtil.get('map');
-		if (container != null) {
-			container._leaflet_id = null;
-		}
-		// getInfo(query);
+		if (L.DomUtil.get('map') != null) L.DomUtil.get('map')._leaflet_id = null;
+
 		var locationIcon = L.icon({
 			iconUrl: 'src/assets/icons/icon-location.svg',
-			iconSize: [46, 56], // size of the icon
-			iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+			iconSize: [40, 50], // size of the icon
+			iconAnchor: [23, 0], // point of the icon which will correspond to marker's location
 		});
 
-		var map = L.map('map').setView([1.668387, -77.011026], 16);
-		L.marker([1.668387, -77.011026], { icon: locationIcon }).addTo(map)
-		// var map = L.map('map').setView([51.505, -0.09], 13);
+		var map = L.map('map').setView([info.location.lat || 43.731567, info.location.lng || 7.414932], 17);
+		L.marker([info.location.lat || 43.731567, info.location.lng || 7.414932], { icon: locationIcon }).addTo(map)
+
 		L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 			maxZoom: 19,
 			attribution: '© OpenStreetMap'
 		}).addTo(map);
 
-		// if (map) {
-		// 	map = map.off();
-		// 	map = map.remove();
-		// }
 	}, []);
 
 	useEffect(() => {
@@ -98,35 +101,26 @@ function App() {
 
 	function handleSubmit(e) {
 		e.preventDefault();
-		console.log(e.target);
+		getInfo(e.target.elements['ip'].value)
 	}
 
 	return (
 		<>
 			<Title>IP Address Tracker</Title>
 			<FormInfo onSubmit={handleSubmit}>
-				<input type="text" placeholder='Search for any IP address or domain' />
+				<input type="text" name='ip' placeholder='Search for any IP address or domain' />
 				<button aria-label='Send'><img src={iconArrow} alt="Send" /></button>
 			</FormInfo>
 
-			<TableInfo>
-				<thead>
-					<tr>
-						<th>IP Address:</th>
-						<th>Location:</th>
-						<th>Timezone:</th>
-						<th>ISP:</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<th>{info.ip}</th>
-						<th>{info.location}</th>
-						<th>UTC {info.timezone}</th>
-						<th>{info.isp}</th>
-					</tr>
-				</tbody>
-			</TableInfo>
+			{info.error
+				? <h1>{info.error || "Oops something is wrong"}</h1>
+				: <TableInfo>
+					<li><span>IP Address:</span><span>{info.ip || "192.212.174.101"}</span></li>
+					<li><span>Location:</span><span>{info.location.region || "Broolyn, NY 10001"}</span></li>
+					<li><span>Timezone:</span><span>UTC {info.location.timezone || "-05:00"}</span></li>
+					<li><span>ISP:</span><span>{info.isp || "SpaceX Starlink"}</span></li>
+				</TableInfo>
+			}
 
 			<div id="map"></div>
 		</>
